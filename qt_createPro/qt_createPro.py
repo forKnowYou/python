0e56ca334c2712630c73e405ea6c2c21e57d2319
forKnowYou <1003425554@qq.com>
2018-05-21 14:01:53 +0800

    add qt_createPro

---
+++ qt_createPro/qt_createPro.py
@@ -0,0 +1,47 @@
+import os
+import sys
+import re
+
+print(sys.argv)
+
+try:
+  if os.path.isdir(sys.argv[1]) or os.path.isdir(sys.argv[2]):
+    filename = './qt_createPro.txt'
+    createPro = open(filename, 'w')
+    rslt = ''
+    
+    if os.path.isdir(sys.argv[1]):
+      rslt += 'INCLUDEPATH += \\\n'
+      files = os.walk(sys.argv[1])
+      for dirpath, dirnames, filenames in files:
+        for i in filenames:
+          if (re.search('\.h$', i) != None) or (re.search('\.hpp$', i) != None):
+            if rslt.count(dirpath) == 0:
+              rslt += '  ' + dirpath + ' \\\n'
+
+    if os.path.isdir(sys.argv[2]):
+      rslt += '\nHEADERS += \\\n'
+      files = os.walk(sys.argv[2])
+      for dirpath, dirnames, filenames in files:
+        for i in filenames:
+          if (re.search('\.h$', i) != None) or (re.search('\.hpp$', i) != None):
+            rslt += '  ' + dirpath + '\\' + i + ' \\\n'
+              
+      rslt += '\nSOURCES += \\\n'
+      files = os.walk(sys.argv[2])
+      for dirpath, dirnames, filenames in files:
+        for i in filenames:
+          print(i)
+          if (re.search('\.c$', i) != None) or (re.search('\.cpp$', i) != None) or (re.search('\.ino$', i) != None):
+            rslt += '  ' + dirpath + '\\' + i + ' \\\n'
+
+    print(rslt)
+
+    createPro.write(rslt)
+    createPro.close()
+
+    os.system('notepad ' + filename)
+    os.remove(filename)
+  
+except:
+  print('path error')
---
+++ qt_createPro/readme.md
@@ -0,0 +1,6 @@
+# QT_createPro
+
+create qt .pro file path text for c/c++ project
+
+first argvment is your library path or null
+second argvment is your project path or null
